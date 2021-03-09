a07c8bc011a0512b3aa33c0355fad3ba15e257be
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2021-03-09 16:01:27 -0500

    Add project files.

---
+++ SignalREx/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalREx.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalREx
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendAllMessage(string userName, string textMessage)
+        {
+            var message = new ChatMessage
+            {
+                UserName = userName,
+                Message = textMessage,
+                TimeStamp = DateTimeOffset.Now
+            };
+
+            await Clients.All.SendAsync("ReceiveMessage",message.UserName, message.TimeStamp,message.Message);
+        }
+    }
+}
